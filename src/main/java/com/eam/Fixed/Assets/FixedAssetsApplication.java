46843b321336d0e0c70c3ad6ac56f93fe28e0146
Arya <91842378+Suryageeks@users.noreply.github.com>
2024-08-18 07:55:09 +0530

    Initial_Spring_Project

---
+++ src/main/java/com/eam/Fixed/Assets/FixedAssetsApplication.java
@@ -0,0 +1,13 @@
+package com.eam.Fixed.Assets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixedAssetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixedAssetsApplication.class, args);
+	}
+
+}
