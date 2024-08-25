ee3b84093ba5e1fd95971bee109afbedcd482d05
Arya Sarkar <arya.08.as44@gmail.com>
2024-08-25 15:26:52 +0530

    ok

--- src/main/java/com/eam/Fixed/Assets/entity/UserRoleSolMapper.java
+++ src/main/java/com/eam/Fixed/Assets/entity/UserRoleSolMapper.java
@@ -1,2 +1,45 @@
-package com.eam.Fixed.Assets.entity;public class userRoleSolMapper {
+package com.eam.Fixed.Assets.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.UUID;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user_role_sol_mapper")
+public class UserRoleSolMapper {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false,columnDefinition = "char(36)")
+    private UUID id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "emp_id",referencedColumnName = "emp_id",nullable = false)
+    private Users users;
+    @Column(name = "emp_name",nullable = false,length = 300)
+    private String empName;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id",referencedColumnName = "role_id",nullable = false)
+    private Roles roles;
+    @Column(name = "role_name", nullable = false, length = 50, unique = true)
+    private String roleName;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sol_id",referencedColumnName = "sol_id",nullable = false)
+    private BranchDetails branchDetails;
+    @Column(name = "branch_name",nullable = false,length = 50)
+    private String branchName;
+    @Column(name = "created_at", nullable = false, updatable = false,columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Timestamp createdAt;
+    @Column(name = "updated_at", nullable = false, insertable = false,columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private Timestamp updatedAt;
+
+    private List<UserRoleSolMapper> userRoleSolMapperList;
 }
--- src/main/java/com/eam/Fixed/Assets/entity/Users.java
+++ src/main/java/com/eam/Fixed/Assets/entity/Users.java
@@ -1,2 +1,41 @@
-package com.eam.Fixed.Assets.entity;public class users {
+package com.eam.Fixed.Assets.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false,columnDefinition = "char(36)")
+    private UUID id;
+    @Column(name = "emp_id",nullable = false,length = 100,unique = true)
+    private String empId;
+    @Column(name = "emp_name",nullable = false,length = 300)
+    private String empName;
+    @Column(name = "address",nullable = false,length = 300)
+    private String address;
+    @Column(name = "state",nullable = false,length = 50)
+    private String state;
+    @Column(name = "district",nullable = false,length = 50)
+    private String district;
+    @Column(name = "mobile_number",nullable = false)
+    private int mobileNumber;
+    @Column(name = "designation",nullable = false,length = 50)
+    private String designation;
+    @Column(name = "created_at", nullable = false, updatable = false,columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Timestamp createdAt;
+    @Column(name = "updated_at", nullable = false, insertable = false,columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private Timestamp updatedAt;
 }
